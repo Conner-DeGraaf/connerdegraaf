2e4dc0a9c1b94fe2878bf0a295682e18c9696aa7
Conner-DeGraaf <connershark2016@gmail.com>
2025-12-04 17:40:08 -0500

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+/// cd portfolio then npm run dev
+
+function App() {
+
+  return (
+    <div>
+      <h1>Conner DeGraaf</h1>
+      <p>Hello, my name is Conner, this is my first vite project. I will be making a portfolio.</p>
+    </div>
+  )
+}
+
+export default App
