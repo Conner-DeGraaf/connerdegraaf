4dce174ed3cd50605151cd7abe98076fc0c520ff
Conner-DeGraaf <connershark2016@gmail.com>
2025-12-05 17:17:23 -0500

    Add files via upload

---
+++ components/Circle.jsx
@@ -0,0 +1,16 @@
+
+// Create the circle for the main page
+export default function Circle(){
+  return(
+  <div className="flex justify-center">
+      <div className="realative h-125 w-125 rounded-full bg-gray-700">
+        
+        <div className="flex flex-col items-center justify-center text-center h-full">
+          <h1 className="font-semibold">Conner DeGraaf</h1>
+          <p className="text-sm">Hello, I am a student at GVSU pursuing a CS degree.</p>
+          <p className="text-sm">Take a look around and get to know me.</p>
+        </div>
+      </div>
+    </div>
+  )
+}
