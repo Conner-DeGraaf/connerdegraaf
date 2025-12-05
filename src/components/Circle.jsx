18732c45caace5b91e4547d50e8fb71d49a3e790
Conner-DeGraaf <connershark2016@gmail.com>
2025-12-05 17:48:18 -0500

    Refactor Circle component for improved styling
    Updated Circle component styles and text sizes for responsiveness.

--- src/components/Circle.jsx
+++ src/components/Circle.jsx
@@ -3,14 +3,17 @@
 export default function Circle(){
   return(
   <div className="flex justify-center">
-      <div className="realative h-125 w-125 rounded-full bg-gray-700">
+      <div className="relative w-80 h-80 lg:w-125 lg:h-125 rounded-full bg-gray-700">
         
         <div className="flex flex-col items-center justify-center text-center h-full">
-          <h1 className="font-semibold">Conner DeGraaf</h1>
-          <p className="text-sm">Hello, I am a student at GVSU pursuing a CS degree.</p>
-          <p className="text-sm">Take a look around and get to know me.</p>
+          <h1 className="font-semibold text-sm lg:text-lg">Conner DeGraaf</h1>
+          <p className="text-xs lg:text-sm">Hello, I am a student at GVSU pursuing a CS degree.</p>
+          <p className="text-xs lg:text-sm">Take a look around and get to know me.</p>
         </div>
+        
       </div>
+    
     </div>
   )
 }
+
